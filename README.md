41abb011f0b755011fc8bbcc4f2913f252c9330b
🀄️ <hongzhongx@users.noreply.github.com>
2024-11-08 20:44:56 +0800

    增加实验NFA“炼天塔”

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # taiyi-contracts
 天道实践，智能游戏脚本的一些样例和实践
+
+# 简单天道合约实验
+
+# 一些实验物品/道具
+
+## 炼天塔
+
+- 天道代码在[nfas/liantianta.lua](./nfas/liantianta.lua)
---
+++ nfas/liantianta.lua
@@ -0,0 +1,24 @@
+heart_beat = { consequence = true }
+active = { consequence = true }
+
+-- 返回一个table
+function init_data()
+    return {        
+        name = "炼天塔",
+        unit = "座"
+    }
+end
+
+function do_active(me, params)
+    nfa_helper:enable_tick()
+end
+
+function do_heart_beat(me, params)
+    -- 转化自身的真气到金石    
+    local nfa = nfa_helper:get_info()
+	-- contract_helper:log(string.format('nfa qi=%d', nfa.qi))
+    if nfa.qi > 1000 then
+        nfa_helper:convert_qi_to_resource(1000, "GOLD")
+    end
+end
+
